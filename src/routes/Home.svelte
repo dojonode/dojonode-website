1d1634e25b638755b9828d1b06c42ec8d215e4b0
wolfez <wolfderechter@gmail.com>
2023-10-20 23:26:51 +0200

    add link to provers

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -14,11 +14,10 @@
     click to enter
   </div>
 </a>
-
 <img src={MilestonesAsset} class="mt-8 hidden md:block" alt="" />
 <div
   class="flex flex-col items-baseline space-y-8 md:space-y-0 md:flex-row md:justify-center md:space-x-16 mt-8 md:mt-0 w-[90%] m-auto text-[#a52519] font-medium"
->
+  >
   <div class="w-full md:w-1/3 p-4">
     <h2 class="md:hidden text-lg font-normal mb-10 text-center text-[#1c7f3f]">
       milestone <span class="block md:block font-bold">01</span>
@@ -128,11 +127,12 @@
 </div>
 
 <div class="footer text-center">
+  <Link to="/provers">---</Link>
   <p>
     made with <img
-      class="w-3 inline align-baseline"
-      src={TaikoHeart}
-      alt="Taiko heart icon"
+    class="w-3 inline align-baseline"
+    src={TaikoHeart}
+    alt="Taiko heart icon"
     /> by
   </p>
   <p>wolfez and helios</p>
@@ -140,18 +140,18 @@
     <a href="https://github.com/wolfderechter/taiko-node-dashboard">
       <span class="github-icon">
         <svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="18"
-          height="18"
-          fill="currentColor"
-          class="bi bi-github"
-          viewBox="0 0 16 16"
+        xmlns="http://www.w3.org/2000/svg"
+        width="18"
+        height="18"
+        fill="currentColor"
+        class="bi bi-github"
+        viewBox="0 0 16 16"
         >
-          <path
-            d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.20-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.20-.82 2.20-.82.44 1.10.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.20 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
-          />
-        </svg>
-      </span>
+        <path
+        d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.20-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.20-.82 2.20-.82.44 1.10.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.20 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"
+        />
+      </svg>
+    </span>
     </a>
   </div>
 </div>
@@ -175,4 +175,4 @@
   .milestone.done {
     text-decoration: line-through;
   }
-</style>
+  </style>
