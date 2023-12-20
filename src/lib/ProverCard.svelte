c394ca7d5d6f4bfb4f42d2f0112eba1f6faa041a
wolfez <wolfderechter@gmail.com>
2023-12-20 21:17:53 +0100

    start working on the prover market page

---
+++ src/lib/ProverCard.svelte
@@ -0,0 +1,72 @@
+<script>
+	// @ts-nocheck
+
+	import KarateIcon from '$lib/images/icons/Karate.png';
+	import TruckIcon from '$lib/images/icons/Truck.png';
+	import TicketIcon from '$lib/images/icons/Ticket.png';
+	import MapIcon from '$lib/images/icons/Map.png';
+
+	// type Endpoint = {
+	//   url: string;
+	//   capacity: number;
+	//   minimumProofFee: number;
+	// };
+
+	// @ts-ignore
+	export let endpoint;
+</script>
+
+<div class="modal shadow-md flex content-between items-center justify-around flex-row">
+			<div class="w-[85px]">
+				<h3 class="modal-title font-bold text-[#E53325]">prover</h3>
+				<img src={KarateIcon} alt="icon" />
+			</div>
+
+      <div class="flex flex-col items-start mt-4 w-[150px] ml-auto card-subbody overflow-clip">
+        <div class="flex flex-row items-center">
+          <img src={TruckIcon} class="icon-small w-8 mr-2" alt="red karate uniform icon" />
+          <div class="flex flex-col text-left">
+            <p class="">capacity</p>
+            <p class="text-[hsl(var(--twc-cardSubBodyColor))]">7</p>
+          </div>
+        </div>
+        <div class="flex flex-row items-center">
+          <img src={TicketIcon} class="icon-small w-8 mr-2" alt="red karate uniform icon" />
+          <div class="flex flex-col text-left">
+            <p class="">min proof fee</p>
+            <p class="text-[hsl(var(--twc-cardSubBodyColor))]">9 wei</p>
+          </div>
+        </div>
+        <div class="flex flex-row items-center">
+          <img src={MapIcon} class="icon-small w-8 mr-2" alt="red karate uniform icon" />
+          <div class="flex flex-col text-left">
+            <p class="">address</p>
+            <p class="truncate text-[hsl(var(--twc-cardSubBodyColor))]">https://192.168.20.1:9876</p>
+          </div>
+        </div>
+      </div>
+</div>
+
+<style>
+	.modal {
+		align-items: center;
+		padding: 10px 20px;
+		margin: 6px;
+		border: 1px solid hsl(var(--twc-cardBackgroundColor));
+		border-radius: 20px;
+		background-color: hsl(var(--twc-cardBackgroundColor));
+		height: 200px;
+    width: 300px;
+	}
+
+	.modal-sub-body {
+		color: hsl(var(--twc-cardSubBodyColor));
+		font-size: 14px;
+	}
+
+	@media (max-width: 750px) {
+		.modal {
+			margin: 4px;
+		}
+	}
+</style>
---
+++ src/routes/tatami/+page.svelte
@@ -0,0 +1,156 @@
+<script>
+	import SplashPageImage from '$lib/images/SplashPage.png';
+	import DashboardThumbnail from '$lib/images/TemporaryThumbnail.png';
+	import DojoLight from '$lib/images/DojoLight.png';
+	import DojoDark from '$lib/images/DojoDark.png';
+	import TatamiDark from '$lib/images/TatamiDark.png';
+	import TatamiLight from '$lib/images/TatamiLight.png';
+	import HeaderImage from '$lib/images/Header.png';
+	import KarateIcon from '$lib/images/icons/Karate.png';
+	import GearIcon from '$lib/images/icons/Gear.png';
+	import NoteIcon from '$lib/images/icons/Note.png';
+	import DojoScrollIcon from '$lib/images/icons/DojoScroll.svg';
+	import KamizaScrollIcon from '$lib/images/icons/KamizaScroll.svg';
+	import TatamiScrollIcon from '$lib/images/icons/TatamiScroll.svg';
+	import ZaScrollIcon from '$lib/images/icons/ZaScroll.svg';
+	import AbacusIcon from '$lib/images/icons/Abacus.png';
+	import MapIcon from '$lib/images/icons/Map.png';
+	import PackageIcon from '$lib/images/icons/Package.png';
+	import LaptopIcon from '$lib/images/icons/Laptop.png';
+	import ThemeSwitcher, { currentTheme } from '$lib/ThemeSwitcher.svelte';
+	import ProverCard from '$lib/ProverCard.svelte';
+
+  let currentThemeValue = '';
+
+  // Change the document body data-theme value on theme changes
+  currentTheme.subscribe((value) => {
+    document.body.setAttribute('data-theme', value);
+    currentThemeValue = value;
+  });
+
+  let endpointExample = {
+    url: 'https://example.com/api',
+    capacity: 100,
+    minimumProofFee: 10,
+  }
+</script>
+
+<svelte:head>
+	<title>prover market page</title>
+	<meta name="description" content="TODO:" />
+</svelte:head>
+
+<header class="flex justify-center w-[90%] m-auto">
+  <a href="/">
+    <img src={HeaderImage} class="max-w-full max-h-[20vh] w-auto h-auto my-8" alt="dojo node header with trees and the logo" />
+  </a>
+</header>
+
+<section>
+	<ThemeSwitcher />
+  <div class="mx-auto relative">
+    <span>
+      <span class="text-[#5CAA80] font-bold">tatami</span>
+      <img src={TatamiScrollIcon} class="icon-big m-auto" alt="tatami flag">
+    </span>
+
+    <div class="card md:absolute md:top-0 md:left-36 w-max text-balance md:text-left mt-8 mb-2 max-w-[20rem]">
+      <h1 class="font-bold">taiko prover market</h1>
+      <h2 class="-mt-2">specialized competition</h2>
+    </div>
+  </div>
+</section>
+
+<section>
+  <div
+      id="cards"
+      class="max-w-[920px] mt-4 flex flex-wrap justify-center overflow-y-clip"
+    >
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <ProverCard
+      endpoint={endpointExample}
+      />
+      <div class="invisible h-5">
+        <ProverCard endpoint={endpointExample}/>
+      </div>
+
+    </div>
+</section>
+
+<style>
+	section {
+    color: hsl(var(--twc-textColor));
+    display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 0.6;
+		width: 50%;
+		margin-left: auto;
+		margin-right: auto;
+		text-align: center;
+	}
+
+  @media only screen and (max-width: 600px) {
+    section {
+      width: 90%;
+    }
+  }
+  @media only screen and (max-width: 1260px) {
+    section {
+      width: 75%;
+    }
+  }
+
+	.card {
+		background-color: hsl(var(--twc-cardBackgroundColor));
+		border-radius: 30px;
+		padding: 10px 20px;
+	}
+
+	.button {
+		background-color: hsl(var(--twc-cardBackgroundColor));
+		border-radius: 30px;
+		padding: 10px 20px;
+	}
+
+	.icon {
+		width: 30px;
+	}
+
+	.icon-small {
+		width: 15px;
+	}
+
+	.icon-big {
+		width: 100px;
+	}
+
+  .card-subbody{
+    color: hsl(var(--twc-cardSubBodyColor));
+    line-height: 1;
+  }
+</style>
